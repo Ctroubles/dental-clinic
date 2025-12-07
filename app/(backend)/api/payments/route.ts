dce94a0d6d17e0804665429d3eeff5910cb1a01e
nstdev0 <nst.dev0@gmail.com>
2025-12-06 19:51:32 -0500

    fix: Payment method filter issue and improve URL filters parsing with nuqs.

--- app/(backend)/api/payments/route.ts
+++ app/(backend)/api/payments/route.ts
@@ -2,22 +2,24 @@ import { NextRequest } from "next/server"
 import { getInjection } from "di/container"
 import { createSecureContext } from "~/lib/api/middleware"
 import { type IGetAllPaymentsUseCaseInput } from "@/application/use-cases/payments/get-all-payments.use-case"
-import { PaymentMethod } from "@/domain/enums"
+import { loadPaymentFilters } from "@/features/payments/helpers/parsers"
 
 export const GET = createSecureContext((req: NextRequest) => {
+  const searchParams = req.nextUrl.searchParams
+
+  const rawInput = loadPaymentFilters(searchParams)
+
   const input: IGetAllPaymentsUseCaseInput = {
     filters: {
-      search: req.nextUrl.searchParams.get("search") || undefined,
-      patientId: req.nextUrl.searchParams.get("patientId") || undefined,
-      doctorId: req.nextUrl.searchParams.get("doctorId") || undefined,
-      chargeId: req.nextUrl.searchParams.get("chargeId") || undefined,
-      visitId: req.nextUrl.searchParams.get("visitId") || undefined,
-      method: req.nextUrl.searchParams.get("method") as
-        | PaymentMethod
-        | undefined,
+      search: rawInput.search,
+      patientId: rawInput.patientId,
+      doctorId: rawInput.doctorId,
+      chargeId: rawInput.chargeId,
+      visitId: rawInput.visitId,
+      method: rawInput.method,
     },
-    page: Number(req.nextUrl.searchParams.get("page")) || 1,
-    pageSize: Number(req.nextUrl.searchParams.get("pageSize")) || 10,
+    page: rawInput.page,
+    pageSize: rawInput.pageSize,
   }
   const getAllPaymentsController = getInjection("IGetAllPaymentsController")
   return getAllPaymentsController(input)
--- src/features/payments/requests.ts
+++ src/features/payments/requests.ts
@@ -2,39 +2,18 @@ import { apiFetch } from "~/lib/api/apiFetch"
 import { PageableResult } from "@/application/common/pagination"
 import { type IGetAllPaymentsUseCaseInput } from "@/application/use-cases/payments/get-all-payments.use-case"
 import { Payment } from "@/domain/entities/payment"
+import { serializePaymentFilters } from "./helpers"
 
-export async function getPayments(request?: IGetAllPaymentsUseCaseInput) {
-  const searchParams = new URLSearchParams()
 
-  if (request?.filters?.search) {
-    searchParams.set("search", request.filters.search)
-  }
-  if (request?.filters?.patientId) {
-    searchParams.set("patientId", request.filters.patientId)
-  }
-  if (request?.filters?.doctorId) {
-    searchParams.set("doctorId", request.filters.doctorId)
-  }
-  if (request?.filters?.chargeId) {
-    searchParams.set("chargeId", request.filters.chargeId)
-  }
-  if (request?.filters?.visitId) {
-    searchParams.set("visitId", request.filters.visitId)
-  }
-  if (request?.filters?.method) {
-    searchParams.set("method", request.filters.method)
+export async function getPayments(rawRequest?: IGetAllPaymentsUseCaseInput) {
+  let queryURL: string = ""
+
+  if (rawRequest) {
+    const request = {...rawRequest, ...(rawRequest.filters || {})}
+    queryURL = serializePaymentFilters(request)
   }
 
-  if (request?.page) {
-    searchParams.set("page", request.page.toString())
-  }
-
-  if (request?.pageSize) {
-    searchParams.set("pageSize", request.pageSize.toString())
-  }
-
-  const queryString = searchParams.toString()
-  const url = queryString ? `payments?${queryString}` : "payments"
+  const url = queryURL ? `payments${queryURL}` : "payments"
 
   const response = await apiFetch<PageableResult<Payment>>(url, {
     method: "GET",
